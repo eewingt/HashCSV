7c74debfaba774dfd90a2506d630c7cbb43b0cf6
eewingt <89181023+eewingt@users.noreply.github.com>
2024-09-04 13:37:47 +0800

    Update combineCSV.py

--- combineCSV.py
+++ combineCSV.py
@@ -3,7 +3,7 @@ import os
 import datetime
 
 # Directory containing the CSV files
-directory = "C:/Users/leslietoh/Downloads/excel"
+directory = "C:/Users/user/Downloads/excel"
 
 # Output file path
 output_file = 'combined_file.csv'
@@ -11,7 +11,7 @@ output_file = 'combined_file.csv'
 # List to hold dataframes
 dfs = []
 
-
+# check if output folder exist, if not exist then create an output folder.
 newpath = r'output'  # This line should be properly aligned with other code
 if not os.path.exists(newpath):
     os.makedirs(newpath)
@@ -41,54 +41,13 @@ if os.path.exists('output'):
     
     # Create the new file name with the date and time
     output_file = f"{'combined_file'}_{timestamp}{file_extension}"
-    
-    #create a output folder
 
     csv_path = os.path.join(newpath, output_file)
     
     # Save the combined dataframe to a new CSV file
     combined_df.to_csv(csv_path, index=False)
     
-    print(f"Folder exists")
+    print(f"All the CSV files within the directory has been merged successfully and saved to output directory.")
 else:
     print("Folder does not exist.")
-
-print(f'Combined file saved to {output_file}')
-
-csv_directory = newpath + '/' + output_file
-
-# Read the CSV file into a DataFrame
-df = pd.read_csv(csv_directory)
-
-# Filter the DataFrame based on the conditions
-filtered_df = df[(df.iloc[:, 1] == 'normal') & (df.iloc[:, 2] == 'SFF')]
-filtered_df1 = df[(df.iloc[:, 1] == 'spam') & (df.iloc[:, 2] == 'SFF')]
-filtered_df2 = df[(df.iloc[:, 1] == 'phish') & (df.iloc[:, 2] == 'SFF')]
-filtered_df3 = df[(df.iloc[:, 1] == 'normal') & (df.iloc[:, 2] == 'DLL')]
-filtered_df4 = df[(df.iloc[:, 1] == 'spam') & (df.iloc[:, 2] == 'DLL')]
-filtered_df5 = df[(df.iloc[:, 1] == 'phish') & (df.iloc[:, 2] == 'DLL')]
-filtered_df6 = df[(df.iloc[:, 1] == 'normal') & (df.iloc[:, 2] == 'MPH')]
-filtered_df7 = df[(df.iloc[:, 1] == 'spam') & (df.iloc[:, 2] == 'MPH')]
-filtered_df8 = df[(df.iloc[:, 1] == 'phish') & (df.iloc[:, 2] == 'MPH')]
-filtered_df9 = df[(df.iloc[:, 1] == 'normal') & (df.iloc[:, 2] == 'HSV')]
-filtered_df10 = df[(df.iloc[:, 1] == 'spam') & (df.iloc[:, 2] == 'HSV')]
-filtered_df11 = df[(df.iloc[:, 1] == 'phish') & (df.iloc[:, 2] == 'HSV')]
-
-
-# Count the number of rows that meet the conditions
-countSFF_normal = filtered_df.shape[0]
-countSFF_spam = filtered_df1.shape[0]
-countSFF_phish = filtered_df2.shape[0]
-countDLL_normal = filtered_df3.shape[0]
-countDLL_spam = filtered_df4.shape[0]
-countDLL_phish = filtered_df5.shape[0]
-countMPH_normal = filtered_df6.shape[0]
-countMPH_spam = filtered_df7.shape[0]
-countMPH_phish = filtered_df8.shape[0]
-countHSV_normal = filtered_df9.shape[0]
-countHSV_spam = filtered_df10.shape[0]
-countHSV_phish = filtered_df11.shape[0]
-
-# Print the result (optional)
-print(f'Number of rows where type is "spam" and domain is "SFF": {countHSV_phish}')
 
